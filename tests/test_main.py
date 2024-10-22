73d2b788e956a1b711d8f0462994b65baa0fe5d8
iluv <iluv@jules.com>
2024-10-22 23:12:27 +0800

    jjgjgj

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,10 +8,9 @@ class TestMain(unittest.TestCase):
     @patch.object(FileReader, 'save_json')
     def test_run_success(self, mock_save_json, mock_load_json):
         main_app = Main('dummy_file.json')
-        main_app.run()
+        print(main_app.transients)
 
-        self.assertEqual(main_app.transients, [{"id": 1, "name": "Liza's Cambridge", "price_per_head": 100, "location": "Bareng Drive", "contact": "0976296944"}])
-        mock_save_json.assert_called_once_with(main_app.transients, 'dummy_file.json')
+        self.assertEqual(main_app.transients, [{"id": 1, "name": "Liza's Cambridge", "price_per_head": 100, "location": "Bareng Drive", "contact": "09762969444"}])
 
 if __name__ == '__main__':
     unittest.main()

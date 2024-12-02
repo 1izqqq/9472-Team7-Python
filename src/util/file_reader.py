57fdd7a794b151f599962bf5b48d83d24ecf45b4
iluv <iluv@jules.com>
2024-12-03 00:20:27 +0800

    Refactored file_reader.py to handle error and renamed variables

--- src/util/file_reader.py
+++ src/util/file_reader.py
@@ -7,17 +7,20 @@ class FileReader:
             with open(file_path, 'r') as file:
                 return json.load(file)
         except FileNotFoundError:
-            print(f"Error: The file '{file_path}' was not found.")
+            FileReader._handle_error(f"The file '{file_path}' was not found.")
         except json.JSONDecodeError:
-            print(f"Error: The file '{file_path}' is not a valid JSON.")
+            FileReader._handle_error(f"The file '{file_path}' is not a valid JSON.")
         return []
 
     @staticmethod
-    def save_json(transients, file_path):
+    def save_json(data, file_path):
         try:
             with open(file_path, 'w') as file:
-                json.dump(transients, file, indent=4)
+                json.dump(data, file, indent=4)
             print(f"Data successfully saved to '{file_path}'")
         except Exception as e:
-            print(f"Error saving to '{file_path}': {e}")
-            raise
+            FileReader._handle_error(f"Error saving to '{file_path}': {e}")
+
+    @staticmethod
+    def _handle_error(message):
+        print(f"Error: {message}")
